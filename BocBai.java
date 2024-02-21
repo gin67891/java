14df61e894a50578229723d4b73bd2dbaf06ba0e
gin67891 <63233908+gin67891@users.noreply.github.com>
2024-02-21 12:25:20 +0700

    Add files via upload

--- BocBai.java
+++ BocBai.java
@@ -6,13 +6,12 @@ public class BocBai {
         Scanner sc = new Scanner(System.in);
         Integer key = 1;
         while (key != 0) {
-            System.out.println("Nhap phim bat ky (Nhap 0 de ket thuc): ");
+            System.out.println("Nhap so bai muon boc (Nhap 0 de ket thuc): ");
             key = sc.nextInt();
-
-            System.out.println("Nhap so bai muon boc: ");
-            int soNhap = sc.nextInt();
-
-            System.out.println("Bai sau khi sap xep: " + BoBai.XepBai(soNhap));
+            if (key > 0 && key < 53) {
+                System.out.println("Bai sau khi sap xep: " + BoBai.XepBai(key));
+            }else
+                System.out.println("Nhap sai, moi nhap lai");
         }
         sc.close();
     }
